d956f7d15204dd3b3185cd3529f2dcdf55f8b1d3
tom.kenji <surume265@gmail.com>
2021-05-14 16:37:41 +0000

    complete

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,69 @@
+class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:edit, :destroy, :show]
+  
+  def index
+    @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+    end
+  end
+
+  def edit
+    current_user.tasks.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    if @task.update(task_params)
+      flash[:success] = 'Task は正常に更新されました'
+      redirect_to @task
+    else
+      @task = Task.find(params[:id])
+      flash.now[:danger] = 'Task は更新されませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+
+    flash[:success] = 'Task は正常に削除されました'
+    redirect_to tasks_url
+  end
+  
+  private
+
+
+
+ 
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+   end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,16 @@
+Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  get 'users/new'
+  get 'users/create'
+  root to: 'tasks#index'
+  
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  resources :tasks
+  get 'signup', to: 'users#new'
+  resources :users, only: [:create]
+end
